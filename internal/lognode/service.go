712350763784b05f7039a2a636d574ab84a760d9
aoiasd <zhicheng.yue@zilliz.com>
2023-09-21 15:15:38 +0800

    1

--- internal/lognode/service.go
+++ internal/lognode/service.go
@@ -18,17 +18,46 @@ package lognode
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/msgpb"
 	"github.com/milvus-io/milvus/internal/proto/logpb"
+	"github.com/milvus-io/milvus/internal/util"
+	"github.com/milvus-io/milvus/pkg/common"
+	"github.com/milvus-io/milvus/pkg/log"
+	"github.com/milvus-io/milvus/pkg/util/commonpbutil"
 	"github.com/milvus-io/milvus/pkg/util/merr"
+	"github.com/milvus-io/milvus/pkg/util/paramtable"
+	"go.uber.org/zap"
 )
 
 func (node *LogNode) WatchChannel(ctx context.Context, req *logpb.WatchChannelRequest) (*commonpb.Status, error) {
-	return merr.Status(nil), nil
+	log.Debug("received WatchChannel Request",
+		zap.Int64("msgID", req.GetBase().GetMsgID()),
+		zap.String("pChannel", req.GetPChannel()))
+
+	if !node.lifetime.Add(commonpbutil.IsHealthy) {
+		msg := fmt.Sprintf("query node %d is not ready", paramtable.GetNodeID())
+		err := merr.WrapErrServiceNotReady(msg)
+		return merr.Status(err), nil
+	}
+	defer node.lifetime.Done()
+
+	if !CheckTargetID(req) {
+		targetID := req.GetBase().GetTargetID()
+		log.Warn("target ID not match",
+			zap.Int64("targetID", targetID),
+			zap.Int64("nodeID", paramtable.GetNodeID()),
+		)
+		return util.WrapStatus(commonpb.ErrorCode_NodeIDNotMatch, common.WrapNodeIDNotMatchMsg(targetID, paramtable.GetNodeID())), nil
+	}
+
+	err := node.loggerManager.AddLogger(ctx, req.GetPChannel())
+	return merr.Status(err), nil
 }
 
 func (node *LogNode) Insert(ctx context.Context, msg *msgpb.InsertRequest) (*commonpb.Status, error) {
+	msg.
 	return merr.Status(nil), nil
 }
